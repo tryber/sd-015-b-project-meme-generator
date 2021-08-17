127a693741c295a9ffc2b163ba84cfadf20dd8c1
davi <davisoarescoura@gmail.com>
2021-08-17 23:57:20 +0200

    adiciona requisito 2,3,4 e 5 do projeto

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 const memeText = document.getElementById('meme-text');
 const textInput = document.getElementById('text-input');
-//const memeImageContainer = document.getElementById('meme-image-container');
 
 textInput.addEventListener('input', textoDoImput);
 
 function textoDoImput(event) {
   memeText.innerText = event.target.value;
+}
+
+window.addEventListener('load', function () {
+    document.querySelector('input[type="file"]')
+    .addEventListener('change', function () {
+      if (this.files && this.files[0]) {
+        var img = document.querySelector('img');
+        img.onload = () => {
+          URL.revokeObjectURL(img.src); // no longer needed, free memory
+        };
+
+        img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+      }
+    });
+});
+
+function toLimit(string = ""){
+    string.value = string.value.substring(0,60);
 }
