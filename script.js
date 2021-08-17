c64a7d791ace55a343afac666fb08d19ad935c8f
Guilherme Saboia <gsaboia@yahoo.com>
2021-08-17 16:08:39 -0300

    Adds requirement 2

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-const input = document.getElementById('text-input');
-const addTextBtn = document.getElementById('add-text');
+const textInput = document.getElementById('text-input');
+const imageInput = document.getElementById('meme-insert');
 const memeText = document.getElementById('meme-text');
+const memeImage = document.getElementById('meme-image');
 
 function addText() {
-  const inputValue = input.value;
+  const inputValue = textInput.value;
   memeText.innerHTML = inputValue;
 }
-input.addEventListener('keyup', addText)
+textInput.addEventListener('keyup', addText);
+
+// Source: https://stackoverflow.com/questions/49942611/how-to-display-a-photo-uploaded-by-user-in-html-and-javascript
+// Source: https://stackoverflow.com/questions/3029422/how-do-i-auto-resize-an-image-to-fit-a-div-container
+function addImage(image) {
+  const reader = new FileReader();
+  // when read operation is finished, onload is triggered, and file is represented as base64 encoded string
+  reader.readAsDataURL(image);
+  reader.onload = function setSource(originEvent) {
+    memeImage.style.height = '100%';
+    memeImage.style.width = '100%';
+    memeImage.style.objectFit = 'contain';
+    memeImage.src = originEvent.target.result;
+  };
+}
+
+// Source: https://betterprogramming.pub/handling-file-inputs-with-javascript-9f2d3a007f05
+function getImageInput() {
+  const selectedImage = imageInput.files[0];
+  if (selectedImage) {
+    addImage(selectedImage);
+  }
+}
+imageInput.addEventListener('change', getImageInput);
