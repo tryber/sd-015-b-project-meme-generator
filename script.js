8171bca2d8f0a04451688205146f8167933b4bd6
anniehau <anniehaurani@gmail.com>
2021-08-17 17:17:08 -0300

    Lint arrumado.

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 // Variáveis
-  const textInput = document.querySelector('#text-input');
-  const imgInput = document.querySelector('#meme-insert')
-  const container = document.querySelector('#meme-image-container');
-  const text = document.querySelector('#meme-text');
-  const img = document.querySelector('#meme-image')
+const textInput = document.querySelector('#text-input');
+const imgInput = document.querySelector('#meme-insert');
+const text = document.querySelector('#meme-text');
+const img = document.querySelector('#meme-image');
 
 // Funções
-  function updateText(event) {
-    text.innerHTML = event.target.value;
-  }
+function updateText(event) {
+  text.innerHTML = event.target.value;
+}
 
-  function updateImg(event) {
-    let newImg = event.target.files[0]
-    img.src = URL.createObjectURL(newImg);
-  }
+function updateImg(event) {
+  const newImg = event.target.files[0];
+  img.src = URL.createObjectURL(newImg);
+}
 
 // Event Listeners
-textInput.addEventListener('input', updateText)
-imgInput.addEventListener('change', updateImg)
+textInput.addEventListener('input', updateText);
+imgInput.addEventListener('change', updateImg);
