4bb4d5e65c827dd2c854eefdb5ea312e3cc5327a
Antonio Souza <antoniofelipe2001@hotmail.com>
2021-08-17 18:29:54 -0300

    2° ajuste de lint

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //utils
 
 function addEventListenerToArray(array, eventType, func) {
-  for (let item of array) {
+  for (const item of array) {
     item.addEventListener(eventType, func);
   }
 }
@@ -9,9 +9,9 @@ function addEventListenerToArray(array, eventType, func) {
 // 7
 
 function createPreMadeImages() {
-  const preMadeImagesContainer = document.querySelector('.pre-made-images-container')
+  const preMadeImagesContainer = document.querySelector('.pre-made-images-container');
   for (let index = 1; index <= 4; index += 1) {
-    img = document.createElement('img');
+    let img = document.createElement('img');
     img.width = 200;
     img.height = 200;
     img.src = `./imgs/meme${index}.png`;
@@ -22,14 +22,14 @@ function createPreMadeImages() {
 }
 
 function selectPreMadeImage(event) {
-  const actualImg = document.getElementById('meme-image')
+  const actualImg = document.getElementById('meme-image');
   const preMadeImg = event.target;
   const preMadeImgSrc = preMadeImg.src;
   if (actualImg) {
     actualImg.src = preMadeImgSrc;
   } else {
     const imgContainer =  document.getElementById('meme-image-container');
-    newImg = document.createElement('img');
+    let newImg = document.createElement('img');
     newImg.height = 500;
     newImg.width = 900;
     newImg.id = 'meme-image';
@@ -51,22 +51,22 @@ function setElementBorder(event) {
 function handleFiles(files) {
   if (files.length) {
     const imgContainer = document.getElementById('meme-image-container');
-    let img = document.createElement('img');
+    const img = document.createElement('img');
     img.src = window.URL.createObjectURL(files[0]);
     img.height = 500;
     img.width = 900;
     img.id = 'meme-image';
-    img.onload = function(e) {
+    img.onload = function () {
       window.URL.revokeObjectURL(this.src);
-    }
+    };
     imgContainer.appendChild(img);
   }
 }
 
 function setMemeImage(event) {
-  let actualImg = document.getElementById('meme-image');
+  const actualImg = document.getElementById('meme-image');
   if (actualImg) {
-    actualImg.remove()
+    actualImg.remove();
     const inputElement = event.target;
     const inputFiles = inputElement.files;
     handleFiles(inputFiles);
@@ -90,14 +90,12 @@ function startEventListeners() {
   const imageInput = document.getElementById('meme-insert');
   imageInput.addEventListener('change', setMemeImage);
   const elementButtons = document.getElementsByClassName('button');
-  addEventListenerToArray(elementButtons, 'click', setElementBorder)
+  addEventListenerToArray(elementButtons, 'click', setElementBorder);
   const preMadeImages = document.getElementsByClassName('pre-made-image');
   addEventListenerToArray(preMadeImages, 'click', selectPreMadeImage);
 }
 
-
-
 window.onload = function () {
   createPreMadeImages();
   startEventListeners();
-}
+};
