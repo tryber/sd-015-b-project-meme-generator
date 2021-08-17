955687182a2572b2b19856c8b4e1cee2bed68315
leandro oliveira <leandrooliveira35@gmail.com>
2021-08-17 18:57:54 -0300

    requisitos 2 ao 5 completos

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ textInput.addEventListener('input', writeInContainer);
 function writeInContainer(event) {
   memeText.innerText = event.target.value;
 }
+
+window.addEventListener('load', () => {
+  document.querySelector('input[type="file"]').addEventListener('change', function () {
+    const image = document.querySelector('img');
+    if (this.files && this.files[0]) {
+      image.onload = () => {
+        URL.revokeObjectURL(image.src);
+      };
+      image.src = URL.createObjectURL(this.files[0]);
+    }
+  });
+});
