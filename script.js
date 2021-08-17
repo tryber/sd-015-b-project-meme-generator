94f0fdded2341b025cbc7bf2e4c116a324baad1d
David Souza <dabcs@poli.ufrj.br>
2021-08-17 19:22:48 -0300

    finalizacao do requisito 7

--- script.js
+++ script.js
@@ -5,16 +5,25 @@ const waterButton = document.getElementById('water');
 const earthButton = document.getElementById('earth');
 const textMeme = document.getElementById('meme-text');
 const conteinerMeme = document.getElementById('meme-image-container');
+const selectMeme1 = document.getElementById('meme-1');
+const selectMeme2 = document.getElementById('meme-2');
+const selectMeme3 = document.getElementById('meme-3');
+const selectMeme4 = document.getElementById('meme-4');
 
 function insertText() {
   textMeme.innerText = inputText.value;
 }
 
-function insertImage() {
-  const id = 'meme-image';
+function removeImage(value) {
+  const id = value;
   if (document.getElementById(id)) {
     document.getElementById(id).remove();
   }
+}
+
+function insertImage() {
+  const id = 'meme-image';
+  removeImage(id);
   if (inputImage.files.length === 1) {
     const img = document.createElement('img');
     img.id = id;
@@ -37,8 +46,42 @@ function changeBorder(element) {
   }
 }
 
+function switching(element, image) {
+  const preElement = element;
+  const img = image;
+  switch (preElement) {
+  case 'meme1':
+    conteinerMeme.appendChild(img);
+    break;
+  case 'meme2':
+    conteinerMeme.appendChild(img);
+    break;
+  case 'meme3':
+    conteinerMeme.appendChild(img);
+    break;
+  case 'meme4':
+    conteinerMeme.appendChild(img);
+    break;
+  default:
+  }
+}
+
+function preMolded(element) {
+  const preElement = element.target.id;
+  const id = 'meme-image';
+  const img = document.createElement('img');
+  img.id = id;
+  img.src = `./imgs/${preElement}.png`;
+  removeImage(id);
+  switching(preElement, img);
+}
+
 inputText.addEventListener('keyup', insertText);
 inputImage.addEventListener('change', insertImage);
 fireButton.addEventListener('click', changeBorder);
 waterButton.addEventListener('click', changeBorder);
 earthButton.addEventListener('click', changeBorder);
+selectMeme1.addEventListener('click', preMolded);
+selectMeme2.addEventListener('click', preMolded);
+selectMeme3.addEventListener('click', preMolded);
+selectMeme4.addEventListener('click', preMolded);
