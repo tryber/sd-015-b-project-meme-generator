1142cbd4b29bc864bc22cc60985a59797c61fa15
leandro oliveira <leandrooliveira35@gmail.com>
2021-08-17 19:56:26 -0300

    requisitos 6 ao 8

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const textInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
+const fire = document.getElementById('fire');
+const water = document.getElementById('water');
+const earth = document.getElementById('earth');
+const memeImageContainer = document.getElementById('meme-image-container');
+const memeCollection = document.getElementById('meme-collection');
+const memeImage = document.getElementById('meme-image');
+console.log(memeCollection);
 
 textInput.addEventListener('input', writeInContainer);
+fire.addEventListener('click', putFireInAll);
+water.addEventListener('click', washedAll);
+earth.addEventListener('click', iLikeGreen);
+memeCollection.addEventListener('click', openMeme);
 
 function writeInContainer(event) {
   memeText.innerText = event.target.value;
@@ -18,3 +29,20 @@ window.addEventListener('load', () => {
     }
   });
 });
+// função adquirida em https://stackoverflow.com/questions/22087076/how-to-make-a-simple-image-upload-using-javascript-html
+
+function putFireInAll() {
+  memeImageContainer.style.border = '3px dashed red';
+}
+
+function washedAll() {
+  memeImageContainer.style.border = '5px double blue';
+}
+
+function iLikeGreen() {
+  memeImageContainer.style.border = '6px groove green';
+}
+
+function openMeme(event) {
+  memeImage.src = event.target.src;
+}
