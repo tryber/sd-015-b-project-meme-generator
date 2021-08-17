005b42b40db77e39d11578a9cb6224cb46bbd4a6
davi <davisoarescoura@gmail.com>
2021-08-18 01:30:07 +0200

    adiciona requisitos 6 e 7 ao projeto

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const memeText = document.getElementById('meme-text');
 const textInput = document.getElementById('text-input');
+const fire = document.getElementById('fire');
+const memeImageContainer = document.getElementById('meme-image-container');
+const water = document.getElementById('water');
+const earth = document.getElementById('earth');
+const memeImage = document.getElementById('meme-image');
+const colecaoMemes = document.getElementById('bibliotecaDeMemes')
 
+fire.addEventListener('click', colocaFogo);
+water.addEventListener('click', jogarAgua);
+earth.addEventListener('click', mapaMundo);
 textInput.addEventListener('input', textoDoImput);
+colecaoMemes.addEventListener('click', openMeme);
 
 function textoDoImput(event) {
   memeText.innerText = event.target.value;
 }
 
 window.addEventListener('load', function () {
-    document.querySelector('input[type="file"]')
+  document
+    .querySelector('input[type="file"]')
     .addEventListener('change', function () {
       if (this.files && this.files[0]) {
         var img = document.querySelector('img');
@@ -21,6 +32,20 @@ window.addEventListener('load', function () {
     });
 });
 
-function toLimit(string = ""){
-    string.value = string.value.substring(0,60);
+function toLimit(string = '') {
+  string.value = string.value.substring(0, 60);
+}
+
+function colocaFogo() {
+  memeImageContainer.style.border = '3px dashed red';
+}
+
+function jogarAgua() {
+  memeImageContainer.style.border = '5px double blue';
+}
+function mapaMundo() {
+  memeImageContainer.style.border = '6px groove green';
+}
+function openMeme(event) {
+  memeImage.src = event.target.src;
 }
