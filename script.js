7d9d2d65296d8a701778418359383a57c507cc52
Antonio Souza <antoniofelipe2001@hotmail.com>
2021-08-17 17:08:24 -0300

    Finalizado requisito 2

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
+function handleFiles(files) {
+  if (files.length) {
+    const imgContainer = document.getElementById("meme-image-container");
+    let img = document.createElement("img");
+    img.src = window.URL.createObjectURL(files[0]);
+    img.height = 500;
+    img.id = "meme-image"
+    img.onload = function(e) {
+      window.URL.revokeObjectURL(this.src);
+    }
+    imgContainer.appendChild(img);
+  }
+}
+
+function setMemeImage(event) {
+  const inputElement = event.target;
+  const inputFiles = inputElement.files;
+  handleFiles(inputFiles);
+}
+
+// 1
 function setMemeText(event) {
-  const textInput = event.target
+  const textInput = event.target;
   const memeText = document.getElementById("meme-text");
-  memeText.innerText = textInput.value
+  memeText.innerText = textInput.value;
 }
 
 function startEventListeners() {
   const textInput = document.getElementById("text-input");
   textInput.addEventListener("keyup", setMemeText)
+  const imageInput = document.getElementById("meme-insert");
+  imageInput.addEventListener("change", setMemeImage);
 }
 
 
