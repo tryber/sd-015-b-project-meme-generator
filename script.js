867cb60110cfa54faab6659a9795180204b7db24
anniehau <anniehaurani@gmail.com>
2021-08-17 17:06:48 -0300

    Exercicio 2 completo.

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 // Variáveis
-  const input = document.querySelector("#text-input");
+  const textInput = document.querySelector("#text-input");
+  const imgInput = document.querySelector("#meme-insert")
   const container = document.querySelector("#meme-image-container");
   const text = document.querySelector("#meme-text");
+  const img = document.querySelector("#meme-image")
 
 // Funções
   function updateText(event) {
-    let memeText = event.value;
     text.innerHTML = event.target.value;
   }
 
+  function updateImg(event) {
+    let newImg = event.target.files[0]
+    img.src = URL.createObjectURL(newImg);
+  }
+
 // Event Listeners
-input.addEventListener('input', updateText)
+textInput.addEventListener('input', updateText)
+imgInput.addEventListener('change', updateImg)
