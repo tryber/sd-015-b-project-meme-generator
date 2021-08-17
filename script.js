0dad67bead0210278ada7e5f83f4f8a3a770dd7a
David Souza <dabcs@poli.ufrj.br>
2021-08-17 16:29:39 -0300

    criacao da funcionalidade de escolher imagem

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const input = document.getElementById('text-input');
+const inputText = document.getElementById('text-input');
+const inputImage = document.getElementById('meme-insert');
 const textMeme = document.getElementById('meme-text');
-
-input.addEventListener("keyup",insertText)
+const conteinerMeme = document.getElementById('meme-image-container');
 
 function insertText() {
-textMeme.innerText = input.value;
+  textMeme.innerText = inputText.value;
 }
+
+function insertImage() {
+  const img = document.createElement('img');
+  img.id = 'meme-image';
+  img.src = URL.createObjectURL(inputImage.files[0]);
+  conteinerMeme.appendChild(img);
+}
+
+inputText.addEventListener('keyup', insertText);
+inputImage.addEventListener('change', insertImage);
