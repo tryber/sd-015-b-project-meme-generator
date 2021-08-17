05e8724d6a8eec77689e593c75adc93725559681
David Souza <dabcs@poli.ufrj.br>
2021-08-17 17:12:39 -0300

    criacao dos botoes para mudar a borda

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const inputText = document.getElementById('text-input');
 const inputImage = document.getElementById('meme-insert');
+const fireButton = document.getElementById('fire');
+const waterButton = document.getElementById('water');
+const earthButton = document.getElementById('earth');
 const textMeme = document.getElementById('meme-text');
 const conteinerMeme = document.getElementById('meme-image-container');
 
@@ -18,5 +21,21 @@ function insertImage() {
   }
 }
 
+function changeBorder(element) {
+  const buttonReference = element.target;
+
+  if (buttonReference.id === 'fire') {
+    conteinerMeme.style.border = '3px dashed red';
+    console.log('teste');
+  } else if (buttonReference.id === 'water') {
+    conteinerMeme.style.border = '5px double blue';
+  } else if (buttonReference.id === 'earth') {
+    conteinerMeme.style.border = '6px groove green';
+  }
+}
+
 inputText.addEventListener('keyup', insertText);
 inputImage.addEventListener('change', insertImage);
+fireButton.addEventListener('click', changeBorder);
+waterButton.addEventListener('click', changeBorder);
+earthButton.addEventListener('click', changeBorder);
