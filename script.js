66f469d9f6c35cce67ed0a6ead1470094ab26ab2
Paulo Flóra <tflora.2004@gmail.com>
2021-08-18 12:57:40 -0300

    Feito o requisito 6.

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const input = document.querySelector('input');
 const textContainer = document.querySelector('#meme-text');
 const uploadBtn = document.getElementById('meme-insert');
 const meme = document.querySelector('img');
+const memeContainer = document.getElementById('meme-image-container');
+const btnContainer = document.getElementById('btn-container');
+// const fireBtn = document.getElementById('fire');
+// const waterBtn = document.getElementById('water');
+// const earthBtn = document.getElementById('earth');
 
 function getText() {
   textContainer.innerHTML = input.value;
@@ -15,3 +20,18 @@ function uploadImg(event) {
 }
 
 uploadBtn.addEventListener('change', uploadImg);
+
+memeContainer.style.border = 'solid 1px black';
+
+function changeBorder(event) {
+  const btn = event.target;
+  if (btn.innerText === 'Verde') {
+    memeContainer.style.border = 'groove 6px green';
+  } else if (btn.innerText === 'Vermelho') {
+    memeContainer.style.border = 'dashed 3px red';
+  } else {
+    memeContainer.style.border = 'double 5px blue';
+  }
+}
+
+btnContainer.addEventListener('click', changeBorder);
