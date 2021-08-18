2b759609b5dd9c350de2c69684e29c40479743b0
alvesgf16 <alvesgabriel1994@gmail.com>
2021-08-17 22:55:08 -0300

    Permite upload de imagens

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-localStorage.clear();
-
 const textInput = document.getElementById('text-input');
 const memeText = document.getElementById('meme-text');
-console.log(textInput, memeText);
 
 function addTexttoMeme() {
   memeText.innerText = textInput.value;
 }
 
 textInput.addEventListener('keyup', addTexttoMeme);
+
+const imageInput = document.getElementById('meme-insert');
+const memeContainer = document.getElementById('meme-image-container');
+
+function addImagetoMeme() {
+  const uploadedImage = imageInput.files[0];
+  const memeImage = document.createElement('img');
+
+  memeImage.id = 'meme-image';
+  memeImage.src = URL.createObjectURL(uploadedImage);
+  memeContainer.appendChild(memeImage);
+}
+
+imageInput.addEventListener('change', addImagetoMeme);
