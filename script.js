c2554449381fd713781fc93df0357ab24648f4d9
Paulo Flóra <tflora.2004@gmail.com>
2021-08-18 12:31:35 -0300

    Corrigido o erro do cypress e feito o requisito 2.

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const input = document.querySelector('input');
 const textContainer = document.querySelector('#meme-text');
+const uploadBtn = document.getElementById('meme-insert');
+const meme = document.querySelector('img');
 
 function getText() {
   textContainer.innerHTML = input.value;
 }
 
 input.addEventListener('keyup', getText);
+
+function uploadImg(event) {
+  const src = URL.createObjectURL(event.target.files[0]);
+  meme.src = src;
+}
+
+uploadBtn.addEventListener('change', uploadImg);
