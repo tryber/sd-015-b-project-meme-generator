af876c345ef2ac8977da944140b30aa44c5bd1a7
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-08-21 03:37:16 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 const textInput = document.getElementById('text-input');
-const memeContainer = document.getElementById('meme-image-container');
 const memeText = document.getElementById('meme-text');
+const memeImage = document.getElementById('meme-image');
 
 function textCreate() {
   memeText.innerText = textInput.value;
 }
 
 textInput.addEventListener('keyup', textCreate);
+
+function imagemContain() {
+  const file = this.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+
+    reader.addEventListener('load', function setImage() {
+      memeImage.setAttribute('src', this.result);
+    });
+
+    reader.readAsDataURL(file);
+  }
+}
+
+const memeInsert = document.getElementById('meme-insert');
+memeInsert.addEventListener('change', imagemContain);
