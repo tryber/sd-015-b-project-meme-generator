ef432c088c2086ab29f1e6b8812f78a6f25be405
Denis Jonathan <denisjonathan7@gmail.com>
2021-08-22 04:52:57 -0300

    Requisito 2 concluído

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const input = document.getElementById('text-input');
-const p = document.getElementById('meme-text');
+const memeText = document.getElementById('meme-text');
+const img = document.getElementById('meme-image');
+const imgPreview = document.getElementById('meme-insert');
 
 function textInput() {
   const inputValue = input.value;
-  p.innerText = inputValue;
+  memeText.innerText = inputValue;
+}
+
+function previewImage() {
+  const file = imgPreview.files;
+  if (file.length > 0) {
+    const fileReader = new FileReader();
+    fileReader.onload = function (event) {
+      img.setAttribute('src', event.target.result);
+    };
+
+    fileReader.readAsDataURL(file[0]);
+    console.log('teste');
+  }
 }
 
 input.addEventListener('keyup', textInput);
+imgPreview.addEventListener('change', previewImage);
