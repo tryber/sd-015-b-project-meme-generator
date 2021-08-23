12bfa2f7744534f497d8b3f0db8acc113a0a6dee
Flávio Bianchetti <fjb.developer@gmail.com>
2021-08-23 07:37:35 -0300

    Teste de requisito 2 - npm esta invalidando

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-window.onload = function pageInit() {
-  inputTextMemePaste();
-  inputTextMemeKeyPress();
-}
-
 function textMeme(event) {
   let paste = (event.clipboardData || window.clipboardData).getData('text');
   const memeText = document.getElementById('meme-text');
@@ -11,7 +6,11 @@ function textMeme(event) {
 
 function textMemeKeyPress(event) {
   const memeText = document.getElementById('meme-text');
-  memeText.innerText = event.target.value + event.key;
+  if (event.key === 'Enter') {
+    memeText.innerText = event.target.value;
+  } else {
+    memeText.innerText = event.target.value + event.key;
+  }
 }
 
 function inputTextMemePaste() {
@@ -22,4 +21,31 @@ function inputTextMemePaste() {
 function inputTextMemeKeyPress() {
   const textInput = document.getElementById('text-input');
   textInput.addEventListener('keypress', textMemeKeyPress);
+}
+
+function getImageFile(event) {
+  const file = event.target.files[0];
+  let reader = new FileReader();
+  const memeImage = document.getElementById('meme-image');
+  memeImage.accept = 'image/jpeg, image/png';
+  reader.onload = function() {
+    memeImage.src = reader.result;
+  }
+  if (file) {
+    reader.readAsDataURL(file);
+    console.log(reader);
+  } else {
+      memeImage.src = '';
+  }
+}
+
+function inputGetImageFile() {
+  const memeInsert = document.getElementById('meme-insert');
+  memeInsert.addEventListener('change', getImageFile);
+}
+
+window.onload = function pageInit() {
+  inputTextMemePaste();
+  inputTextMemeKeyPress();
+  inputGetImageFile();
 }
